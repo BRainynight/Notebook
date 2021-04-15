9de8b78cd1cef506b253f8df23ac28a7896b6f10
Judy <judy1001kitten@gmail.com>
2021-04-15 16:51:58 +0800

    add time flag

--- eval.py
+++ eval.py
@@ -8,10 +8,11 @@ import os
 
 # logger 
 from logpkg import get_logger
-logger = get_logger(fileName="train", timeFlag=True)
+logger = get_logger(fileName="eval", timeFlag=True)
 
+# if you want to eval in cpu , directly se device in cpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("cuda" if torch.cuda.is_available() else "cpu")  
+print("your device is", "cuda" if torch.cuda.is_available() else "cpu")  
 
 
 
@@ -45,29 +46,30 @@ loss_func = nn.MSELoss()
 logger.info("Start to train")
 logger.info(f"total epoch is {EPOCH}, batch size {BATCHSIZE}, learning rate {LR}, model name {MODLE_NAME}")
 
-for epoch in range(EPOCH):
-    model.train()
-    avg_loss = 0
-    for data ,label in dataloader:
-        
-        optimizer.zero_grad()
-        output = model(data.to(device))
-        loss = loss_func(torch.squeeze(output), label.to(device))
 
-        
-        loss.backward()
-        optimizer.step()
-        avg_loss += loss
-        
-        # logger.debug(f"epoch {epoch}, loss={avg_loss:.10f}")
-    avg_loss = avg_loss/len(dataset)
+model.eval()
+avg_loss = 0
 
-    if avg_loss < 0.0005 or epoch > 30:
-        saveWeight(model, name=f"{MODLE_NAME}_{epoch}")
-    logger.info(f"epoch {epoch}, loss={avg_loss:.10f}")
-
-saveWeight(model)
-
-
+# -------------------------------
+# another is put this loop into 
+# with torch.no_grad():
+#   .... some code 
+# -------------------------------
+for data ,label in dataloader:
     
-    
+    optimizer.zero_grad()
+    output = model(data.to(device))
+    loss = loss_func(torch.squeeze(output), label.to(device))
+
+    avg_loss += loss.data.numpy()
+
+avg_loss = avg_loss/len(dataset)
+
+
+''' some graph 
+plt.plot(gt_lt, color='r',alpha=0.5)
+plt.plot(pre_lt, color="b")
+
+plt.savefig(f"eval_{MODLE_NAME}.png")
+logger.info(f"loss={avg_loss/len(dataset):.4f}")
+'''
