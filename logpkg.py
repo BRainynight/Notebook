32de8b15a97e49d32a3710bf85401cdbd8ce119a
Judy <judy1001kitten@gmail.com>
2021-04-15 13:41:38 +0800

    logpkg

---
+++ logpkg.py
@@ -0,0 +1,30 @@
+import logging
+def get_logger(rootName="__main__", child="", logfileName='record.log'):
+    '''
+    直接使用 get_logger ，返回的 logger 不會有層級。
+    1. 根據程式的 package 層級自動設定：要包括參數整行複製去用：get_logger(rootName="__main__", child=__name__)
+    2. 自定義層級：同時覆蓋 rootName, child 自己寫層級架構。get_logger(rootName="parent", child="child1.child2)
+    3. 不使用層級：rootName 可自由選擇是否更改，但 child 請設為空 get_logger(rootName="__main__", child="")
+    '''
+    if not child:
+        logName = rootName
+    else:
+        logName = rootName+"."+child
+    print("Your Log name is ", logName)
+    logger = logging.getLogger(logName) 
+
+    # file handler
+    fh = logging.FileHandler(logfileName,mode='w')
+    fh.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+
+    # put filehandler into logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
